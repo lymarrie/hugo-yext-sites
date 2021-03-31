138c29980c6b9dcf5446937cd08a3bf5378d90c0
lymarrie <16665882+lymarrie@users.noreply.github.com>
2021-03-30 22:08:25 -0400

    updated build.sh file

--- ci/build.sh
+++ ci/build.sh
@@ -1,3 +1,5 @@
-HUGO_VERSION=0.64.0 npx hugo-cli version
-HUGO_VERSION=0.64.0/extended npx hugo-cli version
+HUGO_VERSION=0.64.0
+npx hugo-cli version
+HUGO_VERSION=0.64.0/extended
+npx hugo-cli version
 npx hugo-cli server
